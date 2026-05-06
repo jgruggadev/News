bf776f50b6be8dd9bcc4930426484342756aa371
jgruggadev <jtalbans@gmail.com>
2026-05-06 00:36:35 -0400

    Add files via upload

---
+++ health.js
@@ -0,0 +1,10 @@
+export default function handler(req, res) {
+  res.status(200).json({
+    ok: true,
+    service: 'kelley-news-briefing',
+    time: new Date().toISOString(),
+    endpoints: {
+      briefing: '/api/daily-briefing?key=YOUR_CRON_SECRET'
+    }
+  });
+}
